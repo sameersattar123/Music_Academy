6d7fe73ab99b626a9c064ea06af4b7b18f810657
Sameer <sameersattar1111@gmail.com>
2024-04-28 03:16:23 +0500

    webinar section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestinomialCards from "@/components/TestinomialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <FeaturedCourses/>
     <WhyChooseUs/>
     <TestinomialCards/>
+    <UpcomingWebinars/>
   </main>
   );
 }
