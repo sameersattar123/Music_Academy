9a4e552f0c43b964f16394a6a56a579fcda93aee
Sameer <sameersattar1111@gmail.com>
2024-04-29 01:45:06 +0500

    some changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,26 @@
 "use client";
 import { Meteors } from "@/components/ui/meteors";
-import React, { useState, FormEvent  } from "react";
+import React, { useState, FormEvent } from "react";
 
 const page = () => {
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        console.log(email , message)
-    }
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, message);
+  };
 
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 py-12 pt-36 relative">
-        //     <Meteors number={20} />
+      // <Meteors number={20} />
       <div className="max-w-2xl mx-auto p-4 relative z-10">
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
-          We here to help with any questions about our courses,
-          programs, or events. Reach out and let us know how we can assist you
-          in your musical journey.
+          We here to help with any questions about our courses, programs, or
+          events. Reach out and let us know how we can assist you in your
+          musical journey.
         </p>
         <form onSubmit={handleSubmit} className="space-y-4 mt-4">
           <input
